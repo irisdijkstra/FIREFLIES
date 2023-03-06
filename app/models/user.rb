e8bfea78a8a67c51d20bd1fa21255db983c5b86f
Iris D <iris.dijkstra@hotmail.com>
2023-03-06 16:31:21 +0100

    has_many relations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :messages
+  has_many :fireflies, through: :messages
 end
