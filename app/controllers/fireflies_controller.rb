83e434a52c50426b8eb0b2dba6b364f4efa79cc2
Iris D <iris.dijkstra@hotmail.com>
2023-03-07 12:43:00 +0100

    Firefly controller updated (not done yet) + seeds

--- app/controllers/fireflies_controller.rb
+++ app/controllers/fireflies_controller.rb
@@ -1,17 +1,43 @@
 class FirefliesController < ApplicationController
 
-  def create
-    @message = Message.where(message_id: params[:message_id])
-
-    # in costum route:
+  def new
+    # in final version this should be in costum route:
     @firefly = Firefly.new
+  end
 
-    # here
-    @firefly = Firefly.create!(firefly_params)
+  def create
+    @message = Message.find(params[:message_id])
+    @firefly = Firefly.new(firefly_params)
+    @firefly.message = @message
+    # @message.user = current_user
+
+    raise
+
+    # if @firefly.save!
+    #   redirect_to root_path
+    # else
+    #   render ""
+    # end
+
+    # @chatroom = Chatroom.find(params[:chatroom_id])
+    # @message = Message.new(message_params)
+    # @message.chatroom = @chatroom
+    # @message.user = current_user
+    # if @message.save
+    #   redirect_to chatroom_path(@chatroom)
+    # else
+    #   render "chatrooms/show", status: :unprocessed_entity
+    # end
+
   end
 
   def show
     @firefly = Firefly.find(params[:id])
+  end
+
+  def destroy
+    @firefly = Firefly.find(params[:id])
+    @firefly.destroy!
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+require 'faker'
+
+# Destroy all seeds first before craeting new ones
+Firefly.destroy_all
+Message.destroy_all
+User.destroy_all
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +12,38 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Creating 10 seeds...'
+
+10.times do
+  first_name = Faker::Name.first_name
+  date = Faker::Date.between(from: '2021-03-07', to: '2025-03-07')
+
+  user = User.new(
+    first_name: first_name,
+    last_name: Faker::Name.last_name,
+    email: "#{first_name}@firefly.com",
+    password: 111111
+  )
+  user.save!
+
+  message = Message.new(
+    letter: Faker::Movie.quote,
+    # image: nil,
+    # video: nil,
+    # audio: nil
+    user_id: user.id
+    )
+  message.save!
+
+  firefly = Firefly.new(
+    date_sent: date,
+    date_recieve: (date+365),
+    email_recipient: Faker::Internet.email,
+    message_id: message.id
+  )
+  firefly.save!
+end
+
+puts ""
+puts 'Done!'
