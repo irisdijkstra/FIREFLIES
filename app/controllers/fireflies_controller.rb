eebbcef85550c474e33fe536f9d80671450d15fa
Iris D <iris.dijkstra@hotmail.com>
2023-03-08 11:17:25 +0100

    Firefly create

--- app/controllers/fireflies_controller.rb
+++ app/controllers/fireflies_controller.rb
@@ -11,8 +11,8 @@ class FirefliesController < ApplicationController
 
   def new
     # in final version this should be in costum route:
-    @firefly = Firefly.new
     @message = Message.find(params[:message_id])
+    @firefly = Firefly.new
   end
 
   def index
@@ -23,19 +23,15 @@ class FirefliesController < ApplicationController
     @message = Message.find(params[:message_id])
 
     @message.email_to.each do |receiver|
-      @firefly = Firefly.new(firefly_params)
-      @firefly.email_recipient = receiver
+      @firefly = Firefly.new(email_recipient: receiver)
       @firefly.message = @message
-      @firefly.save!
-    end
-    # @firefly.email_recipient = @message.email_to.first
-      @message.user = current_user # sender
-      # @message.save!
+      @firefly.update(firefly_params)
 
-    if @firefly.save!
-      redirect_to root_path
-    else
-      render "fireflies/form", status: :unprocessed_entity
+      if @firefly.save!
+        puts "firefly to #{receiver} saved!"
+      else
+        puts "firefly to #{receiver} is not working"
+      end
     end
   end
 
@@ -51,6 +47,6 @@ class FirefliesController < ApplicationController
   end
 
   def firefly_params
-    params.require(:firefly).permit(:date_sent, :date_recieve, :email_recipient)
+    params.require(:firefly).permit(:date_sent, :date_recieve)
   end
 end
