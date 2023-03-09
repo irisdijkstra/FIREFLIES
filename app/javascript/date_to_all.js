93532a1c92daf1c339468a4b4e1fa811e7f5c146
Iris D <iris.dijkstra@hotmail.com>
2023-03-09 18:08:28 +0100

    form-dates with ajax logics (not done yet)

--- app/javascript/date_to_all.js
+++ app/javascript/date_to_all.js
@@ -1,6 +1,5 @@
 
 const dateToAll = (event) => {
-  const formAll = document.querySelector("#form-all");
   const dateAll = document.querySelector("#firefly-date-recieve-all");
   const applyAllBtn = document.querySelector("#apply-all-btn");
 
@@ -11,6 +10,8 @@ const dateToAll = (event) => {
   const formActions = document.querySelectorAll(".simple_form")
   const sendBtn = document.querySelector("#send-btn")
 
+  if(applyAllBtn) {
+    // when clicked, main date will be applied to all fireflies
     applyAllBtn.addEventListener("click", () => {
       dateEach.forEach((date) => {
         if (date.value == dateAll.value){
@@ -21,35 +22,35 @@ const dateToAll = (event) => {
         };
       });
     });
+  };
 
+  if(costumizeBtn) {
+    // when clicked, costumize dates will be shown for different fireflies
     costumizeBtn.addEventListener("click", () => {
       formEach.classList.toggle('d-none');
     });
-
-    // sendBtn.addEventListener("click", () => {
-    //   // if clicked, all dates will be saved for fireflies
-    //   //console.log(sendBtn)
-    //   //console.log(formActions)
-
-    //   formActions.forEach((form) => {
-    //     //const csrfToken = document.querySelector("[name='csrf-token']").content;
-    //     //console.log(csrfToken);
-
-    //     //console.log(form.action)
-
-    //     // This will display the date for (each) form
-    //     //console.log(form.querySelector(".date-form").value)
-
-    //     fetch(form.action, {
-    //       method: "PATCH",
-    //       headers: { "Accept": "text/plain"},
-    //       body: new FormData(form)
-    //     })
-    //     .then((response) => response.text())
-    //     .then((data) => console.log(data))
-    //   });
-
-    // });
   };
+
+  if(sendBtn) {
+    sendBtn.addEventListener("click", () => {
+    // if clicked, all dates will be saved for fireflies
+      formActions.forEach((field) => {
+        const csrfToken = document.querySelector("[name='csrf-token']").content;
+        // let costumMail = field.querySelector("#firefly_email_recipient")
+        // let costumDate = field.querySelector(".date-form").value
+
+      // (field.action) => this is the URL to where the firefly will be patched
+        fetch(field.action, {
+          method: "PATCH",
+          headers: { "Accept": "text/plain"},
+          body: new FormData(formActions)
+        })
+        .then((response) => response.text())
+        .then((data) => console.log(data))
+        });
+
+      });
+    };
+};
 
 export { dateToAll }
