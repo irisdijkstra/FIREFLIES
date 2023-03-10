bc0f707b2c87cdf315651f5cf857488bcdb9ac00
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-03-10 15:45:27 +0100

    seedwith images

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,12 @@ Message.destroy_all
 User.destroy_all
 
 puts 'Creating 10 seeds...'
-first_name = ["Paola", "Ana", "Iris", "Evgeniya", "Danko"]
+first_name = ["paola", "ana", "iris", "evgeniya", "danko"]
 index = 0
+file = "https://www.billboard.com/wp-content/uploads/2022/03/23.-Miles-Davis-%E2%80%98Bitches-Brew-1969-album-art-billboard-1240.jpg?w=1024"
 
 5.times do
-  date = Faker::Date.between(from: '2021-03-07', to: '2025-03-07')
+  date = Faker::Date.between(from: '2023-03-04', to: '2023-03-08')
 
   user = User.new(
     first_name: first_name[index],
@@ -22,16 +23,17 @@ index = 0
   user.save!
   message = Message.new(
     letter: Faker::Movie.quote,
-    # image: nil,
     # video: nil,
     # audio: nil
     user_id: user.id,
     email_to: ["#{first_name[index - 1]}@firefly.com"]
     )
+    message.image.attach(io: URI.open(file), filename: "bench.png", content_type: "image/png")
+
   message.save!
   firefly = Firefly.new(
     date_sent: date,
-    date_recieve: (date + 365),
+    date_recieve: (date + 1),
     email_recipient: "#{first_name[index - 1]}@firefly.com",
     message_id: message.id
   )
