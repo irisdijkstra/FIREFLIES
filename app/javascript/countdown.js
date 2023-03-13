2a7ae579b1addb14a7322b37fbf7410c10adb429
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-03-13 14:24:21 +0100

    countdown prototype

--- app/javascript/countdown.js
+++ app/javascript/countdown.js
@@ -1,14 +1,23 @@
 const countdown = () => {
-  // const deadline
-  // const total = deadline - Date.now
-  const dead = document.querySelector("#timer")
+  const clock = document.getElementById("clock")
   const deadline = document.querySelector("#timer").dataset.date
   const today = Date.now()
   const waiting = new Date(deadline).getTime() - today
 
-  // let days = Math.floor(dista)
-  console.log(waiting)
+  let days = Math.floor(waiting / (1000 * 60 * 60 * 24))
+  let hours = Math.floor((waiting % (1000 * 60 * 60 )) / (1000 * 60 * 60 * 24));
+  let minutes = Math.floor((waiting % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((waiting % (1000 * 60)) / 1000);
+  console.log(deadline)
   console.log(today)
+  console.log(waiting)
+  console.log(days)
+  console.log(hours)
+  console.log(minutes)
+  console.log(seconds)
+
+  clock.innerHTML = days + " days " + hours + " hours "+ minutes + " minutes " + seconds + " seconds ";
+
   new Date(deadline).getTime()
 }
 
