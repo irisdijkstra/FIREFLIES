0172841c0949aabdbd9c606d5e584b27a68c4698
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-03-13 14:37:16 +0100

    countdown is moving

--- app/javascript/countdown.js
+++ app/javascript/countdown.js
@@ -1,13 +1,22 @@
 const countdown = () => {
   const clock = document.getElementById("clock")
   const deadline = document.querySelector("#timer").dataset.date
-  const today = Date.now()
-  const waiting = new Date(deadline).getTime() - today
+
+  let x = setInterval(function() {
+
+  var now = new Date().getTime();
+  const waiting = new Date(deadline).getTime() - now
 
   let days = Math.floor(waiting / (1000 * 60 * 60 * 24))
   let hours = Math.floor((waiting % (1000 * 60 * 60 )) / (1000 * 60 * 60 * 24));
   let minutes = Math.floor((waiting % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((waiting % (1000 * 60)) / 1000);
+
+  clock.innerHTML = days + " days " + hours + " hours "+ minutes + " minutes " + seconds + " seconds ";
+
+}, 1000);
+
+
   console.log(deadline)
   console.log(today)
   console.log(waiting)
@@ -15,10 +24,6 @@ const countdown = () => {
   console.log(hours)
   console.log(minutes)
   console.log(seconds)
-
-  clock.innerHTML = days + " days " + hours + " hours "+ minutes + " minutes " + seconds + " seconds ";
-
-  new Date(deadline).getTime()
 }
 
 export { countdown }
