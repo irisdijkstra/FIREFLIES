b170dad408a7c800594c115cb95ce46c2fdc0dfa
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-03-14 15:10:53 +0100

    add color to FAQ style

--- app/javascript/countdown.js
+++ app/javascript/countdown.js
@@ -6,11 +6,8 @@ const countdown = () => {
 
   var now = new Date().getTime();
   const waiting = new Date(deadline).getTime() - now
-  // console.log(new Date(waiting).getTime())
-
 
   const dateMili = new Date(deadline).getTime()
-  // console.log(new Date(dateMili).toString())
 
 
   let days = Math.floor(waiting / (1000 * 60 * 60 * 24))
@@ -25,14 +22,6 @@ const countdown = () => {
 
 
 }, 1000);
-
-
-  console.log(deadline)
-  console.log(waiting)
-  console.log(days)
-  console.log(hours)
-  console.log(minutes)
-  console.log(seconds)
 }
 
 export { countdown }
