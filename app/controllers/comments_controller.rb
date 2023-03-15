9ef40f67591274155771e39652a080ddd8cc6711
Paola Llerena Schiffmacher <p.llerena91@gmail.com>
2023-03-15 10:52:01 +0100

    next changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,43 @@
 class CommentsController < ApplicationController
-  before_action :set_fireflies, only: %i[new create]
+  before_action :set_comment
+  before_action :find_fly
+
+  def index
+    @comments = Comment.all
+  end
+
+  def show
+  end
+
   def new
-    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @firefly.comment = @comment
+    if @comment.save!
+      # redirect_to firefly_path(@comment)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content, :message_id)
   end
 
-  def set_fireflies
+  def find_fly
     @firefly = Firefly.find(params[:firefly_id])
   end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  # def find_message
+  #   @message = Message.find(params[:message_id])
+  # end
 end
--- app/controllers/fireflies_controller.rb
+++ app/controllers/fireflies_controller.rb
@@ -3,6 +3,7 @@ class FirefliesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show], raise: false
 
   def show
+    @comment = Comment.new
   end
 
   def date
