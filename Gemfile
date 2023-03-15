1f2b137193491c60751e2707b6d1b85a905640be
Ana Calija <ana.salevic@gmail.com>
2023-03-15 15:12:57 +0100

    mails continued

--- Gemfile
+++ Gemfile
@@ -54,8 +54,7 @@ gem "autoprefixer-rails"
 gem "font-awesome-sass", "~> 6.1"
 gem "simple_form", github: "heartcombo/simple_form"
 gem "cloudinary"
-gem "sidekiq"
-gem "sidekiq-failures", "~> 1.0"
+
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,11 +2,6 @@
 class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/welcome
-  def welcome
-    # receiver = "beribakdanko@gmail.com"
-    email = params[:email]
-    # UserMailer.with(email: email).welcome.deliver_now
-    UserMailer.with(email: email).welcome.deliver_later
-  end
+
 
 end
