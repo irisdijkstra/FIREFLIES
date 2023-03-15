9d42c38f12dd20f982bff5a220d89648b8f09e9f
Iris D <iris.dijkstra@hotmail.com>
2023-03-15 12:24:28 +0100

    before nested rows

--- app/javascript/messages.js
+++ app/javascript/messages.js
@@ -1,11 +1,11 @@
 const addMail = () => {
   const addMailsButton = document.querySelector("#add-mails-btn");
   const addMailsContainer = document.querySelector(".add-mails");
-  const mailTag = `<input type='text' name='emails[]' class='add-mails-input'>`;
+  const mailTag = `<div class="add-mails" type="text" name="emails[]" class="btn-form-blue add-mails-input">`;
 
   if(addMailsButton) {
     addMailsButton.addEventListener("click", () => {
-      addMailsContainer.insertAdjacentHTML("afterbegin", mailTag);
+      addMailsContainer.insertAdjacentHTML("afterend", mailTag);
     });
   }
 }
