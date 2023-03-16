494a7b229cd9d961bef3fc32b3f2a970ce5f85ca
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-03-16 11:10:06 +0100

    fix cloudinary image upload

--- app/javascript/date_to_all.js
+++ app/javascript/date_to_all.js
@@ -51,7 +51,7 @@ const dateToAll = (event) => {
         // .then((data) => console.log(data))
         // console.log(new FormData(field))
       });
-      window.location.href = "http://www.fireflies.buzz/timeline"
+      window.location.href = "http://www.localhost3000/timeline"
     });
   };
 };
