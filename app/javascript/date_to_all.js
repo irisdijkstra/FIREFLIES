256c600a0029ff64cf4c515fb0b4206b814c8d5c
Paola Llerena Schiffmacher <p.llerena91@gmail.com>
2023-03-16 17:14:07 +0100

    redirection

--- app/javascript/date_to_all.js
+++ app/javascript/date_to_all.js
@@ -51,7 +51,7 @@ const dateToAll = (event) => {
         // .then((data) => console.log(data))
         // console.log(new FormData(field))
       });
-      window.location.href = "http://localhost3000/timeline"
+      window.location.href = "http://www.fireflies.buzz/timeline"
       // "http://www.fireflies.buzz/timeline"
       // "http://www.localhost3000/timeline"
     });
