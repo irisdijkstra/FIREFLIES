cae5c8958616f4f3f9a7948e6a4403ce452f8384
Paola Llerena Schiffmacher <p.llerena91@gmail.com>
2023-03-16 12:59:17 +0100

    user on comments

--- app/javascript/date_to_all.js
+++ app/javascript/date_to_all.js
@@ -51,7 +51,8 @@ const dateToAll = (event) => {
         // .then((data) => console.log(data))
         // console.log(new FormData(field))
       });
-      window.location.href = "http://www.fireflies.buzz/timeline"
+      window.location.href = "http://www.localhost3000/timeline"
+      // "http://www.fireflies.buzz/timeline"
     });
   };
 };
